e9d1f938043930b35b5d0065847e7ff4c5c90064
Deyson Venté <deiandresvent@hotmail.com>
2024-12-23 20:20:19 -0500

    Convertir temperaturas

---
+++ Exercises/Problems.cs
@@ -0,0 +1,27 @@
+﻿namespace appExercises03.Exercises
+{
+    public static class Problems
+    {
+        private static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+        public static void FahrenheitToCelsius(int temperature)
+        {
+            //*5/9 => 0.55555...
+            double celsius = (temperature - 32) * 5 / 9;
+            Console.WriteLine($"La temperatura final en Celsius es: {celsius}");
+        }
+        public static void CelsiusToFa(int temperature)
+        {
+            var result = (temperature * 1.8) + 32;
+            PrintMessage($"El resultado es: {result}");
+        }
+
+        public static void KelvinToCelsius(int temperature)
+        {
+            var result = temperature - 273.15;
+            PrintMessage($"El resultado es: {result}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using appExercises03.Exercises;
+
 Console.WriteLine("Hello, World!");
 
+//Convertir 212 F a celsius
+Problems.FahrenheitToCelsius(-40);
+Problems.KelvinToCelsius(50);
+Problems.CelsiusToFa(-40);
 
-
-//*Evitar que se cierre el programa
+//Evitar que se cierre el programa
 Console.ReadKey();
